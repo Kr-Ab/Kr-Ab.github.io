9de82443ca555522f82df61ff025e89560e86203
Kr-Ab <krabhimanyu2000@gmail.com>
2020-06-25 17:41:02 +0530

    Updates

--- precache-manifest.379f8ba8b85232de1ba557e9ad5e8ec6.js
+++ precache-manifest.379f8ba8b85232de1ba557e9ad5e8ec6.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "df4ee2c81f2a40076b6da4d5066e9fc2",
+    "revision": "c358cf0d09dca3e789f8df48c8a7d6b4",
     "url": "/index.html"
   },
   {
-    "revision": "b1365aac5e53330a7d84",
-    "url": "/static/css/main.523f09c5.chunk.css"
+    "revision": "b2c57f32c7c1ad51a9b6",
+    "url": "/static/css/main.f1b42223.chunk.css"
   },
   {
     "revision": "fac12d52899df9f5652e",
@@ -16,15 +16,11 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.c2f2c482.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b1365aac5e53330a7d84",
-    "url": "/static/js/main.fbde0003.chunk.js"
+    "revision": "b2c57f32c7c1ad51a9b6",
+    "url": "/static/js/main.2f1811a2.chunk.js"
   },
   {
     "revision": "3daaf060e7e784b4d4eb",
     "url": "/static/js/runtime-main.6342a3c8.js"
-  },
-  {
-    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
-    "url": "/static/media/logo.ee7cd8ed.svg"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e775573559269869ce877e5fbdb09e53.js"
+  "/precache-manifest.379f8ba8b85232de1ba557e9ad5e8ec6.js"
 );
 
 self.addEventListener('message', (event) => {
